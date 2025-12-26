51d276ccf7c9a0c15bbcb8c868ef36b2c1b4ace3
parthsudani-7 <sudaniparth80@gmail.com>
2025-12-26 04:55:49 +0000

    changes

---
+++ JAVA/GeeksForGeeks/Decision_Making_in_Java.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static String compareNM(int n, int m) {
+       
+        if(n == m){
+            return "equal";
+        }else if(n > m){
+            return "greater"; 
+        }else{
+            return "lesser";
+        }
+    }
+}
