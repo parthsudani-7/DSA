b15aded60ace574420bd41325b4dcd0a54a1bd07
parthsudani-7 <sudaniparth80@gmail.com>
2025-12-26 05:29:43 +0000

    changes

---
+++ JAVA/GeeksForGeeks/008 Java Switch Case statement.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    static double switchCase(int choice, List<Double> arr) {
+        switch (choice) {
+            case 1:
+                return Math.PI * arr.get(0) * arr.get(0);
+            case 2:
+                return arr.get(0) * arr.get(1);
+            default:
+                return 0.0;
+        }
+    }
+}
