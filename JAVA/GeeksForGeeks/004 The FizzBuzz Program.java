74eb93ffcd52c70f6338da1da09e3b7c5cf79010
parthsudani-7 <sudaniparth80@gmail.com>
2025-12-26 05:19:02 +0000

    changes

---
+++ JAVA/GeeksForGeeks/004 The FizzBuzz Program.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static void fizzBuzz(int number) {
+        if(number % 3 == 0){
+            if(number % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else{
+                System.out.println("Fizz");
+            }
+        }else if(number % 5 == 0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(number);
+        }
+        
+    }
+}
