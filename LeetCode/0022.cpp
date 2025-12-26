a9b41617b96320b8ac0aa74ec95871da0b587564
parthsudani-7 <sudaniparth80@gmail.com>
2025-12-26 12:40:51 +0000

    changes

---
+++ LeetCode/0022.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void solve(string cur, int open, int close, int n) {
+        if (cur.length() == 2 * n) {
+            ans.push_back(cur);
+            return;
+        }
+
+        if (open < n) {
+            solve(cur + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            solve(cur + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        solve("", 0, 0, n);
+        return ans;
+    }
+};
