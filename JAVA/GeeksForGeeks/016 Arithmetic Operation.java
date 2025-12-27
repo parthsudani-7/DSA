430786a7f9c4ab51d5cd8afc68f2babe3938bcb9
parthsudani-7 <sudaniparth80@gmail.com>
2025-12-27 12:47:31 +0000

    stagged changes

---
+++ JAVA/GeeksForGeeks/016 Arithmetic Operation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] computeOperations(int x, int y) {
+        return new int[]{
+            x + y,
+            x - y,
+            x * y,
+            x / y,
+            x % y
+        };
+    }
+}
---
+++ JAVA/GeeksForGeeks/017 vovel or not .java
@@ -0,0 +1,9 @@
+class Solution {
+    String isVowel(char c) {
+        c = Character.toLowerCase(c);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return "YES";
+        }
+        return "NO";
+    }
+}
