327b13501cacad0f5b35afe82dc8cbe591706d08
parthsudani-7 <sudaniparth80@gmail.com>
2025-12-28 13:12:41 +0000

    changes

---
+++ C++/fruit count.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int count(int n, int arr[],int H){
+    int min = 0;
+    int max = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    int mid = min + (max - min)/2;
+
+    while(min < max ){
+        for(int j = 0; j <)
+    }
+}
+
+
+int  main(){
+    count()
+}
