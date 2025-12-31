dd35d0c360a871af6b889e997a079e1852c19341
parthsudani-7 <sudaniparth80@gmail.com>
2025-12-31 03:53:14 +0000

    changes

---
+++ C++/CodeForces/0339A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    string nums;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != '+') {
+            nums.push_back(s[i]);
+        }
+    }
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.length(); i++) {
+        cout << nums[i];
+        if (i != nums.length() - 1)
+            cout << "+";
+    }
+
+    return 0;
+}
