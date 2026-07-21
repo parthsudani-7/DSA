0c5218788e2a389d2b6c4e9f0af853ca93be0226
Parth Sudani <sudaniparth80@gmail.com>
2026-07-21 10:54:28 +0530

    Create Sol.java

---
+++ JAVA/CodeForces/B - Twins/Sol.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      int sum = 0;
+      for(int i = 0; i < n; i++){
+        arr[i] = sc.nextInt();
+        sum += arr[i];
+      }
+      Arrays.sort(arr);
+      int total = 0;
+      int idx = 0;
+      for(int i = n - 1; i >= 0; i--){
+        total += arr[i];
+        idx++;
+        if(total > sum / 2){
+            System.out.println(idx);
+            break;
+
+        }
+      }
+    }
+}
