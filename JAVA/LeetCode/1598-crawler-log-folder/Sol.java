277be9b11ad396ddceb726511d8268d16e35f9f3
Parth Sudani <sudaniparth80@gmail.com>
2026-07-23 13:22:37 +0530

    Add solution for minimum log operations
    Implement a solution to count minimum operations for log navigation.

---
+++ JAVA/LeetCode/1598-crawler-log-folder/Sol.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> st = new Stack<>();
+
+        for(int i = 0; i < logs.length; i++){
+
+            if(logs[i].equals("../")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+                
+            }
+            else if(logs[i].equals("./")){
+                
+            }else{
+                st.push(logs[i]);
+            }
+        }
+        int count = 0;
+        while(!st.isEmpty()){
+            count++;
+            System.out.println(st.peek());
+            st.pop();
+        }
+        return count;
+    }
+}
