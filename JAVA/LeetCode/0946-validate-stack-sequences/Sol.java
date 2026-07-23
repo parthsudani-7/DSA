7c6f27b3d758c0bc5cfbeb96cd80237c83f43bbc
Parth Sudani <sudaniparth80@gmail.com>
2026-07-23 11:09:52 +0530

    Create Sol.java

---
+++ JAVA/LeetCode/0946-validate-stack-sequences/Sol.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        Stack<Integer> st = new Stack<>();
+        int j = 0;
+
+        for (int num : pushed) {
+            st.push(num);
+
+            while (!st.isEmpty() && st.peek() == popped[j]) {
+                st.pop();
+                j++;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
